fdfd12c8a612db86f317983ac689f406f1900b41
pradokez <pradokeziah@gmail.com>
2023-04-02 16:52:55 -0300

    feat: add input

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import React from 'react';
+import './App.css'
 
 function App() {
+  return (
+    <div class="input-group mb-3 custom-width">
+      <input
+        type="text"
+        class="form-control"
+        placeholder="Search Recipe"
+        aria-label="input search"
+        aria-describedby="button-addon2"
+      />
+      <button
+        class="btn btn-outline-secondary"
+        type="button"
+        id="button-addon2"
+      >
+        Button
+      </button>
+    </div>
+  );
 }
 
 export default App;
